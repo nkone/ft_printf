ac7d392d828aa5ecfbe861c12c8c2e50fe2cdd02
nkone <phtruong@protonmail.com>
2019-05-26 17:02:04 -0700

    moved get unbr size from main, revert get_nbr_space

--- print_unbr_size_2.c
+++ print_unbr_size_2.c
@@ -1,57 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   print_nbr_util_2.c                                 :+:      :+:    :+:   */
+/*   print_unbr_size_2.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/22 18:47:34 by phtruong          #+#    #+#             */
-/*   Updated: 2019/05/26 13:35:51 by phtruong         ###   ########.fr       */
+/*   Created: 2019/05/26 10:31:43 by phtruong          #+#    #+#             */
+/*   Updated: 2019/05/26 10:31:55 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int			flag_to_index(int flag)
+uintmax_t	print_unbr_sizelf(va_list ap)
 {
-	int i;
+	uintmax_t n;
 
-	i = 0;
-	while (flag)
-	{
-		flag >>= 1;
-		i++;
-		if (!flag)
-			break ;
-	}
-	return (i);
+	n = va_arg(ap, unsigned int);
+	return (n);
 }
 
-int			get_nbr_len(uintmax_t n)
+uintmax_t	print_unbr_sizej(va_list ap)
 {
-	int len;
+	uintmax_t n;
 
-	if (!n)
-		return (1);
-	len = 0;
-	while (n)
-	{
-		len++;
-		n /= 10;
-	}
-	return (len);
+	n = va_arg(ap, uintmax_t);
+	return (n);
 }
 
-intmax_t	print_nbr_getsize_arg(t_print *p)
+uintmax_t	print_unbr_sizez(va_list ap)
 {
-	intmax_t	n;
-	va_list		ap;
-	int			tmp;
+	uintmax_t n;
 
-	tmp = p->argv;
-	va_copy(ap, p->ap);
-	while (tmp-- > 0)
-		n = g_signed_tab[flag_to_index(p->size)](ap);
-	va_end(ap);
+	n = va_arg(ap, size_t);
+	return (n);
+}
+
+uintmax_t	print_unbr_sizet(va_list ap)
+{
+	uintmax_t n;
+
+	n = va_arg(ap, ssize_t);
 	return (n);
 }
--- print_unbr_util_1.c
+++ print_unbr_util_1.c
@@ -6,7 +6,7 @@
 /*   By: phtruong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 12:54:35 by phtruong          #+#    #+#             */
-/*   Updated: 2019/05/24 14:40:22 by phtruong         ###   ########.fr       */
+/*   Updated: 2019/05/26 13:45:34 by phtruong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,4 +51,25 @@ void	process_sp_plus_flag_unbr(t_print *p)
 		ft_putchar('+');
 	else if (p->flag & _F_SPACE)
 		ft_putchar(' ');
+}
+
+int		get_unbr_space(t_print *p, uintmax_t n, int pads, int len)
+{
+	int space;
+
+	space = 0;
+	if (p->width > 0)
+		(n < 0 || p->flag & _F_PLUS || p->flag & _F_SPACE) && p->width--;
+	if (p->flag & _F_PCN)
+	{
+		if (pads)
+			space = (p->width > (pads + len)) ? (p->width - (pads + len)) : 0;
+		else if (n)
+			space = (p->width >= len) ? (p->width - len) : 0;
+		else if (!n)
+			space = (p->width >= len) ? (p->width - len + 1) : 0;
+	}
+	else
+		space = (p->width > len) ? (p->width - len) : 0;
+	return (space);
 }
